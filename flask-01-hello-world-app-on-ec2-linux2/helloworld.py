ebbbf95416d1da99da789eb2a5952d7f70393bd5
rafe_stefano <stefanorafe@gmail.com>
2020-07-21 07:04:03 +0300

    last version after review

--- flask-01-hello-world-app-on-ec2-linux2/helloworld.py
+++ flask-01-hello-world-app-on-ec2-linux2/helloworld.py
@@ -2,12 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')             # Decorator connect app to the path
 def hello():
-    return "Hello World"
+    return "Hello World from Rafe Stefano"
 
 if __name__=='__main__':
     #  app.run('localhost', port=5000, debug=True)
     # app.run(debug=True)
-    # need to push
-    app.run('0.0.0.0', port=80)
+    app.run('0.0.0.0', port=80)   # Can be accessable from anywhere, port 80(HTTP)
