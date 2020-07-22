d31ce4b2e1356826d05a385dfd0466edb5af2733
rafe_stefano <stefanorafe@gmail.com>
2020-07-22 06:51:13 +0300

    Flask3 first part is done

--- flask-03-handling-forms-and-sql-on-ec2-linux2/app-form-handling.py
+++ flask-03-handling-forms-and-sql-on-ec2-linux2/app-form-handling.py
@@ -1,5 +1,5 @@
 # Import Flask modules
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # Create an object named app
 app = Flask(__name__)
@@ -12,13 +12,31 @@ def home():
     return render_template("index.html", name="Rafe")
 
 # Write a function named `greet` which uses template file named `greet.html` given under `templates` folder
-# and assign to the dynamic route of ('/<name>')
+# @app.route("/<name>", methods=["GET"])
+# def greet(name):
+#     return render_template("greet.html", user=name)
+# Write a function named `greet` which uses template file named `greet.html` given under `templates` folder
+# and assign to the static route of ("/greet")
+@app.route("/greet", methods=["GET"])
+def greet():
+    if "user" in request.args:
+        usr=request.args["user"]
+        return render_template("greet.html", user=usr)
+    else:
+        return render_template("greet.html", user="Send your username with user parameter in query string")
 
 # Write a function named `login` which uses `GET` and `POST` methods,
 # and template files named `login.html` and `secure.html` given under `templates` folder
 # and assign to the static route of ('login')
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        user_name=request.form["username"]
+        return render_template("secure.html", user=user_name)
+    else:
+        return render_template("login.html", )
 
 # Add a statement to run the Flask application which can be reached from any host on port 80.
 if __name__=="__main__":
-    app.run(debug=True)
-    # app.run("0.0.0.0", port=80)
+    # app.run(debug=True)                 # curl -v localhost:5000
+    app.run("0.0.0.0", port=80)
